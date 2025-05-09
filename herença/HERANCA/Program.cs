7327863e469e46650f535d08653bff243b64911f
Eric Ferraz <ericferraz4@gmail.com>
2025-05-08 21:16:54 -0300

    adiciona herança

---
+++ herença/HERANCA/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace HerancaVeiculos
+{
+    
+    class Veiculo
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+
+        public Veiculo(string marca, string modelo)
+        {
+            Marca = marca;
+            Modelo = modelo;
+        }
+
+        public virtual void MostrarInfo()
+        {
+            Console.WriteLine($"Veículo: {Marca} {Modelo}");
+        }
+
+        public virtual void Acelerar()
+        {
+            Console.WriteLine("O veículo está acelerando...");
+        }
+    }
+
+    
+    class Carro : Veiculo
+    {
+        public Carro(string marca, string modelo) : base(marca, modelo)
+        {
+        }
+
+        public override void MostrarInfo()
+        {
+            Console.WriteLine($"Carro: {Marca} {Modelo}");
+        }
+
+        public override void Acelerar()
+        {
+            Console.WriteLine("O carro acelera  pela estrada.");
+        }
+    }
+
+    
+    class Moto : Veiculo
+    {
+        public Moto(string marca, string modelo) : base(marca, modelo)
+        {
+        }
+
+        public override void MostrarInfo()
+        {
+            Console.WriteLine($"Moto: {Marca} {Modelo}");
+        }
+
+        public override void Acelerar()
+        {
+            Console.WriteLine("A moto acelera no corredor.");
+        }
+    }
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Veiculo meuCarro = new Carro("Toyota", "Corolla");
+            Veiculo minhaMoto = new Moto("Yamaha", "MT-07");
+
+            meuCarro.MostrarInfo();
+            meuCarro.Acelerar();
+
+            Console.WriteLine();
+
+            minhaMoto.MostrarInfo();
+            minhaMoto.Acelerar();
+        }
+    }
+}
